2e4de0ec8fd3b4973589da0785262d76e0a2851d
Tamás Györkis <tamasgyorkis@gmail.com>
2023-03-19 19:32:08 +0100

    added teams migration
    Creates teams table

---
+++ scrum_planner/database/migrations/2023_03_19_181653_create_teams_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('team_name');
+            $table->unsignedBigInteger('scrum_master');
+            $table->foreign('scrum_master')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teams');
+    }
+};
