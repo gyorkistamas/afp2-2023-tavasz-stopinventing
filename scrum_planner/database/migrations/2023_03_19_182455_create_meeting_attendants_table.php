5cf39e2a3bb5e33ac9e30f5a934293d26595ee87
Tamás Györkis <tamasgyorkis@gmail.com>
2023-03-19 19:33:11 +0100

    added meeting_attendants migration
    Creates meeting_attendants table.

---
+++ scrum_planner/database/migrations/2023_03_19_182455_create_meeting_attendants_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meeting_attendants', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('meeting_id');
+            $table->unsignedBigInteger('user_id');
+            $table->smallInteger('participate');
+            $table->foreign('meeting_id')->references('id')->on('meetings');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meeting_attendants');
+    }
+};
