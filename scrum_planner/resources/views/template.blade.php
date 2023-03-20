f82f5634f9ea9d114dbcd93e7af39040b70c12d1
Tamás Györkis <tamasgyorkis@gmail.com>
2023-03-20 15:05:02 +0100

    Added base bootstrap navbar

--- scrum_planner/resources/views/template.blade.php
+++ scrum_planner/resources/views/template.blade.php
@@ -5,12 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    @yield('custom_css')
     <link rel="stylesheet" type="text/css" href="{{ url('assets/css/bootstrap.min.css') }}">
     <script src="{{ url('assets/js/bootstrap.js') }}"></script>
 
     @yield('title')
 </head>
 <body>
-    <h1 class="bg-secondary">Test</h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link" href="#">Features</a>
+              <a class="nav-link" href="#">Pricing</a>
+            </div>
+          </div>
+        </div>
+      </nav>
 </body>
 </html>
