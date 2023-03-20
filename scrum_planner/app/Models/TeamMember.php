d623179c5273141e56b3b08e8d72ee6bc1b74c63
Tamás Györkis <tamasgyorkis@gmail.com>
2023-03-20 14:23:52 +0100

    Added TeamMember model
    Added model and fillable fields

---
+++ scrum_planner/app/Models/TeamMember.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TeamMember extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'team_id',
+        'user_id'
+    ];
+}
