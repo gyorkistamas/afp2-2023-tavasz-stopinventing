be5859c798823d9e7eeb872979c46c054d579417
Balázs Karácsony (H8HYMB) <karacsonybalazs01@gmail.com>
2023-03-20 14:31:15 +0100

    Created template blade view

---
+++ scrum_planner/resources/views/template.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/bootstrap.min.css') }}">
+    <script src="{{ url('assets/js/bootstrap.js') }}"></script>
+
+    @yield('title')
+</head>
+<body>
+    <h1 class="bg-secondary">Test</h1>
+</body>
+</html>
