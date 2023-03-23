5ed3a3c42310797116e4efd2544dcf04a9c65938
Tamás Györkis <tamasgyorkis@gmail.com>
2023-03-23 15:24:23 +0100

    modified template
    Modified colors

--- scrum_planner/resources/views/template.blade.php
+++ scrum_planner/resources/views/template.blade.php
@@ -7,25 +7,35 @@
 
     @yield('custom_css')
     <link rel="stylesheet" type="text/css" href="{{ url('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{url('assets/css/template.css')}}">
     <script src="{{ url('assets/js/bootstrap.js') }}"></script>
 
     @yield('title')
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary bg-secondary navbar-dark" data-bs-theme="dark">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <a class="navbar-brand" href="#"><img src="{{url('logo.jpg')}}" alt="Logo image" id="navbar-logo" class="d-inline-block align-text-top">{{env('COMPANY')}}</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-              <a class="nav-link" href="#">Features</a>
-              <a class="nav-link" href="#">Pricing</a>
-            </div>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
+
+
+      @yield('content')
 </body>
 </html>
