fe8d22b336678afec15ade60e8197c7568024131
Balázs Karácsony (H8HYMB) <karacsonybalazs01@gmail.com>
2023-03-27 15:12:27 +0200

    Recreated Users Page ui

--- scrum_planner/resources/views/users/list.blade.php
+++ scrum_planner/resources/views/users/list.blade.php
@@ -7,74 +7,58 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="main-box clearfix">
-                <div class="table-responsive">
-                    <table class="table text-white">
-                        <thead>
-                            <tr>
-                                <th class="text-center"><span>User</span></th>
-                                <th class="text-center"><span>Title</span></th>
-                                <th class="text-center"><span>Created</span></th>
-                                <th class="text-center"><span>Status</span></th>
-                                <th class="text-center"><span>Email</span></th>
-                                <th>&nbsp;</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($Users as $user)
-                        <tr>
-                            <td>
 
-                                <img src={{ $user->picture }} width="50px" height="50px" alt="">
-                                <a href="#">{{ $user->full_name }}</a>
+<div class="container p-5 text-white mb-5">
+    <div class="row justify-content-center gap-3">
+        @foreach ($Users as $user)
+        <div class="card bg-dark" style="max-width: 20rem;">
 
-                            </td>
-                            <td class="text-center">
-                                @if ( $user->privilage == 0)
-                                    <span>Simple User</span>
-                                @elseif ( $user->privilage == 1 )
-                                    <span>Scrum Master</span>
-                                @else
-                                    <span>Admin</span>
-                                @endif
-                            </td>
-                            <td class="text-center">
-                                {{ $user->created_at }}
-                            </td>
-                            <td class="text-center">
-                                <span class="label label-default">Active</span>
-                            </td>
-                            <td class="text-center">
-                                <a href="#">{{$user->email}}</a>
-                            </td>
-                            <td style="width: 20%;">
-                                <a href="#" class="table-link">
-                                    <button class="btn btn-primary">Show</button>
-                                </a>
-                                <a href="#" class="table-link">
-                                    <button class="btn btn-success">Edit</button>
-                                </a>
-                                <a href="#" class="table-link danger">
-                                    <button class="btn btn-warning">Suspend</button>
-                                </a>
-                            </td>
-                        </tr>
-                        @endforeach
-
-                        </tbody>
-                    </table>
-
-                    <div class="row">
-                        <div class="col-12">
-                            {{ $Users->links('pagination::bootstrap-4') }}
-                        </div>
-                    </div>
-
-                </div>
+            <div class="card-header">
+                <span class="badge bg-primary">Status</span>
             </div>
+
+            <img class="card-img-top" src="{{ $user->picture }}" alt="" style="max-width: 20rem">
+
+            <div class="card-body">
+
+                <h5 class="card-title">
+                    {{ $user->full_name }}
+                </h5>
+
+                <p class="card-text font-weight-bold">
+                    {{ $user->email }}
+                </p>
+
+                <p class="card-text font-weight-bold">
+                    <span class="badge bg-secondary">
+                        @if ($user->privilage == 0)
+                            Simple User
+                        @elseif ($user->privilage == 1)
+                            Scrum Master
+                        @else
+                            Admin
+                        @endif
+                    </span>
+                </p>
+
+                <p class="card-text font-weight-bold">
+                    <span class="badge bg-info">{{ $user->created_at }}</span>
+                </p>
+
+                <div class="card-footer d-flex justify-content-between">
+                    <button class="btn btn-warning">Suspend</button>
+                    <button class="btn btn-success">Modify</button>
+                </div>
+
+            </div>
+        </div>
+        @endforeach
+    </div>
+
+
+    <div class="row p-5">
+        <div class="col-12">
+            {{ $Users->links('pagination::bootstrap-5') }}
         </div>
     </div>
 </div>
