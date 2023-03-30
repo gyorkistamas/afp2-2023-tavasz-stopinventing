f3fd0aaf0f252bda8c015f8f7f411ec172dac118
gyorkistamas <tamasgyorkis@gmail.com>
2023-03-30 20:06:39 +0200

    web.php - added route to add participants to meeting

--- scrum_planner/routes/web.php
+++ scrum_planner/routes/web.php
@@ -30,3 +30,4 @@ Route::post('/sign-up',[UserController::class, 'SignUp']);
 Route::get('/meeting/show/{meeting}', [MeetingController::class, 'ShowMeeting'])->middleware('auth');
 Route::post('/meeting/comment', [MeetingController::class, 'RecordComment']);
 Route::post('/meeting/remove-participant', [MeetingController::class, 'RemoveParticipant']);
+Route::post('/meeting/add-participants', [MeetingController::class, 'AddParticipants']);
