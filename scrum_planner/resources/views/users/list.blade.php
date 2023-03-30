9c67ece66c7123ef2d00d4f9ae4ae136e6a6d2b0
Balázs Karácsony (H8HYMB) <karacsonybalazs01@gmail.com>
2023-03-30 18:23:56 +0200

    Updated 'users' page ui

--- scrum_planner/resources/views/users/list.blade.php
+++ scrum_planner/resources/views/users/list.blade.php
@@ -10,49 +10,55 @@
 
 <div class="container p-5 text-white mb-5">
     <div class="row justify-content-center gap-3">
+
         @foreach ($Users as $user)
-        <div class="card bg-dark" style="max-width: 20rem;">
+            <div class="card bg-dark" style="max-width: 20rem;">
 
-            <div class="card-header">
-                <span class="badge bg-primary">Status</span>
-            </div>
-
-            <img class="card-img-top" src="{{ $user->picture }}" alt="" style="max-width: 20rem">
-
-            <div class="card-body">
-
-                <h5 class="card-title">
-                    {{ $user->full_name }}
-                </h5>
-
-                <p class="card-text font-weight-bold">
-                    {{ $user->email }}
-                </p>
-
-                <p class="card-text font-weight-bold">
-                    <span class="badge bg-secondary">
-                        @if ($user->privilage == 0)
-                            Simple User
-                        @elseif ($user->privilage == 1)
-                            Scrum Master
-                        @else
-                            Admin
-                        @endif
-                    </span>
-                </p>
-
-                <p class="card-text font-weight-bold">
-                    <span class="badge bg-info">{{ $user->created_at }}</span>
-                </p>
-
-                <div class="card-footer d-flex justify-content-between">
-                    <button class="btn btn-warning">Suspend</button>
-                    <button class="btn btn-success">Modify</button>
+                <div class="card-header">
+                    @if ($user->status == 0)
+                        <span class="badge bg-primary">Active</span>
+                    @else
+                        <span class="badge bg-warning text-dark">Suspended</span>
+                    @endif
                 </div>
 
+                <img class="card-img-top" src="{{ asset($user->picture) }}" alt="" style="max-width: 20rem">
+
+                <div class="card-body">
+
+                    <h5 class="card-title text-center">
+                        {{ $user->full_name }}
+                    </h5>
+
+                    <p class="card-text font-weight-bold text-center">
+                        {{ $user->email }}
+                    </p>
+
+                    <p class="card-text font-weight-bold text-center">
+                        <span class="badge bg-secondary">
+                            @if ($user->privilage == 0)
+                                Simple User
+                            @elseif ($user->privilage == 1)
+                                Scrum Master
+                            @else
+                                Admin
+                            @endif
+                        </span>
+                    </p>
+
+                    <p class="card-text font-weight-bold text-center">
+                        <span class="badge bg-info">{{ $user->created_at }}</span>
+                    </p>
+
+                    <div class="card-footer d-flex justify-content-between">
+                        <a href="/users/suspend/{{ $user->id }}" class="btn btn-warning">Suspend</a>
+                        <a href="#" class="btn btn-success">Modify</a>
+                    </div>
+
+                </div>
             </div>
-        </div>
         @endforeach
+
     </div>
 
 
