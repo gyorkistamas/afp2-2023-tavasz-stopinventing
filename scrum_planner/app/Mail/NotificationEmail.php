b4b8ece0d7aab249f0790e30500731222eea6959
BirGer05 <gergobirinyi@gmail.com>
2023-04-02 21:11:13 +0200

    Full functional mail

--- scrum_planner/app/Mail/NotificationEmail.php
+++ scrum_planner/app/Mail/NotificationEmail.php
@@ -2,23 +2,32 @@
 
 namespace App\Mail;
 
+use App\Models\User;
+use App\Models\Meeting;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use App\Models\MeetingAttendant;
 use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class NotificationEmail extends Mailable
 {
+    protected Meeting $meeting;
+    protected User $attendant;
     use Queueable, SerializesModels;
 
     /**
      * Create a new message instance.
      */
-    public function __construct()
+    public function __construct(
+        Meeting $meeting,
+        User $attendant
+        )
     {
-        //
+        $this->meeting = $meeting;
+        $this->attendant = $attendant;
     }
 
     /**
@@ -38,6 +47,15 @@ class NotificationEmail extends Mailable
     {
         return new Content(
             view: 'mail.created_meeting',
+            with: [
+                'meetingName' => $this->meeting->name,
+                'startTime' => $this->meeting->start_time,
+                'endTime' => $this->meeting->end_time,
+                'organiser' => $this->meeting->organiser,
+                'description' => $this->meeting->description,
+                'siteName' => env('APP_NAME'),
+                'userName' => $this->attendant->user_id
+            ]
         );
     }
 
