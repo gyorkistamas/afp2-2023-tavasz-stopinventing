bb5d0750c8671efd229ef676294cf01338076c94
BirGer05 <gergobirinyi@gmail.com>
2023-04-03 14:39:21 +0200

    Created the team notification

---
+++ scrum_planner/app/Mail/TeamNotification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class TeamNotification extends Mailable
+{
+    public Team $team;
+    public User $member;
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(
+        Team $team,
+        User $member
+        )
+    {
+        $this->team = $team;
+        $this->member = $member;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Team Notification',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.added_to_a_team',
+            with: [
+                'userName' => $this->member->full_name,
+                'teamName' => $this->team->team_name,
+                'scrumMaster' => $this->team->scrumMaster->full_name,
+                'siteName' => env('APP_NAME')
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
