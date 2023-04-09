f6c91f7ad72cd32193258c05e1b3570ab5125d14
gyorkistamas <tamasgyorkis@gmail.com>
2023-04-09 14:41:43 +0200

    web.php - added route to delete meeting

--- scrum_planner/routes/web.php
+++ scrum_planner/routes/web.php
@@ -59,6 +59,7 @@ Route::post('/meeting/add-participants', [MeetingController::class, 'AddParticip
 
 Route::get('/meeting/edit/{meeting}', [EditMeetingController::class, 'ShowEditForm'])->middleware('auth');
 Route::post('/meeting/edit/{meeting}', [EditMeetingController::class, 'EditMeeting'])->middleware('auth');
+Route::get('/meeting/delete/{meeting}', [EditMeetingController::class, 'DeleteMeeting'])->middleware('auth');
 
 Route::get('/my-meetings', [MeetingController::class, 'MyMeetingsThisWeek'])->middleware('auth')->name('my-meetings');
 Route::get('/my-meetings/{date}', [MeetingController::class, 'MyMeetings'])->middleware('auth');
