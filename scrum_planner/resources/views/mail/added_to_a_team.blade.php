d2092d4cd9e125f84d084fbcf244d4cb73afac02
BirGer05 <gergobirinyi@gmail.com>
2023-04-13 11:54:05 +0200

    Issue #16 fixed

--- scrum_planner/resources/views/mail/added_to_a_team.blade.php
+++ scrum_planner/resources/views/mail/added_to_a_team.blade.php
@@ -1,9 +1,9 @@
 <h1>Greetings, {{$userName}}</h1>
 <h3>You have been added to a team on {{$siteName}}.</h3>
-<h3>-----------------------------------------------------------</h3>
+<h1></h1>
 <h2><b>Team Name: </b>{{$teamName}}</h2>
 <h2><b>Scrum master : </b>{{$scrumMaster}}</h2>
-<h3>-----------------------------------------------------------</h3>
+<h1></h1>
 <h3>Now you can check it on the site!</h3>
 <p></p>
 <p>This is an automated email, please do not respond to this e-mail address!</p>
