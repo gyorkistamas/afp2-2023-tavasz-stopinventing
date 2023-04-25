13cfb1ae017b89faca2b636bdcf7e5b8e2ef6b9b
imretab <imre.tabajdi@gmail.com>
2023-04-25 17:01:45 +0200

    Updated randompwd - added function for showbutton

--- scrum_planner/public/assets/js/randompwd.js
+++ scrum_planner/public/assets/js/randompwd.js
@@ -2,3 +2,11 @@ function GenerateRndPasswd(id){
     let pwdField = document.getElementById("newPwd"+id);
     pwdField.value = Math.random().toString(36).slice(2, 10);
 }
+function pwd2text(id){    
+    let toText = document.getElementById("newPwd"+id);
+    toText.type="text";
+}
+function back(id){
+    let toText = document.getElementById("newPwd"+id);
+    toText.type="password";
+}
