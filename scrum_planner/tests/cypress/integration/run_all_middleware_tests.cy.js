4530e9fe2365c3abda9e087a27521b891ce02677
gyorkistamas <tamasgyorkis@gmail.com>
2023-04-29 18:12:23 +0200

    cypress - middleware tests

---
+++ scrum_planner/tests/cypress/integration/run_all_middleware_tests.cy.js
@@ -0,0 +1,4 @@
+import './test_middlewares_as_guests.cy';
+import './test_middlewares_as_normal_user.cy';
+import './test_middlewares_as_scrum_master.cy';
+import './test_middleware_as_admin.cy';
---
+++ scrum_planner/tests/cypress/integration/test_middleware_as_admin.cy.js
@@ -0,0 +1,101 @@
+describe('middleware test for admin', () => {
+    before(() => {
+        cy.refreshDatabase();
+        cy.seed();
+
+    });
+
+    beforeEach(() => {
+        cy.login({email:'adminuser@justacompany.com'});
+    });
+
+    it('visits the sign in page', () => {
+        cy.visit('/sign-in', {failOnStatusCode: false});
+        cy.contains('404')
+    });
+
+    it('visits the sign up page', () => {
+        cy.visit('/sign-up', {failOnStatusCode: false});
+        cy.contains('404')
+    });
+
+    it('visits the forgotten password page', () => {
+        cy.visit('/request-password', {failOnStatusCode: false});
+        cy.contains('404')
+    });
+
+    it('visits the users page', () => {
+        cy.visit('/users', {failOnStatusCode: false});
+        cy.contains('Admin user')
+    });
+
+    it('visits the status change page', () => {
+        cy.visit('/users/change-status/1', {failOnStatusCode: false});
+        cy.contains('Suspended')
+    });
+
+    it('visits the team creation page', () => {
+        cy.visit('/team/create', {failOnStatusCode: false});
+        cy.contains('Create new team')
+    });
+
+    it('visits the manage teams page', () => {
+        cy.visit('/manage-teams', {failOnStatusCode: false});
+        cy.contains('Search')
+    });
+
+    it('visits the edit team page', () => {
+        cy.visit('/team/edit/1', {failOnStatusCode: false});
+        cy.contains('404')
+    });
+
+    it('visits the delete team page', () => {
+        cy.visit('/team/delete/1', {failOnStatusCode: false});
+        cy.contains('404')
+    });
+
+    it('visits the edit profile page', () => {
+        cy.visit('/edit-profile', {failOnStatusCode: false});
+        cy.contains('Edit Profile')
+    });
+
+    it('visits the meeting creation page', () => {
+        cy.visit('/meeting/create', {failOnStatusCode: false});
+        cy.contains('Meeting Creation')
+    });
+
+    it('visits the meeting edit page', () => {
+        cy.visit('/meeting/edit/1', {failOnStatusCode: false});
+        cy.contains('404')
+    });
+
+    it('visits the meeting delete page', () => {
+        cy.visit('/meeting/delete/1', {failOnStatusCode: false});
+        cy.contains('404')
+    });
+
+    it('visits a show meeting page', () => {
+        cy.visit('/meeting/show/1', {failOnStatusCode: false});
+        cy.contains('404')
+    });
+
+    it('visits the my meetings page', () => {
+        cy.visit('/my-meetings');
+        cy.contains('Next week')
+    });
+
+    it('visits the my invites page', () => {
+        cy.visit('/my-invites');
+        cy.contains('Back to my meetings')
+    });
+
+    it('visits the manage meetings page', () => {
+        cy.visit('/manage-meetings', {failOnStatusCode: false});
+        cy.contains('Created Meetings')
+    });
+
+    it('visits the sign out page', () => {
+        cy.visit('/sign-out');
+        cy.contains('Sign In')
+    });
+})
